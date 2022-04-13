8e24c7ff256e89b5c166284af8aed01961ab9e54
Hakimi keerai <hakeemee222@gmail.com>
2022-04-13 08:29:02 +0700

    structure

---
+++ portfolio101/components/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <div>
+      <div className=" text-center my-5 text-white text-5xl ">
+        Hi, I'm keemee
+      </div>
+      <p className=" text-center font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Perspiciatis architecto, facere quibusdam tempore quod perferendis voluptatibus.</p>
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ portfolio101/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className=" bg-black w-full h-10  text-lg flex justify-center space-x-5 ">
+      <div>About Me</div>
+      <div>My Skill</div>
+      <div>Contact</div>
+      
+    </div>
+  );
+}
+
+export default NavBar;
